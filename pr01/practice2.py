7430a9cfba899888695ab8b16536ea11093e2221
eunbb <fakesilverpark@gmail.com>
2025-03-14 23:48:47 +0900

    2차시 수업 복습2

---
+++ pr01/practice2.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple, Dict, Optional, Union
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+def process_number(numbers: List[int]) -> List[int]:
+    new_numbers = [number * 2 for number in numbers]
+    return new_numbers
+
+def get_person_info() -> Tuple[str, int]:
+    return ("Park", 18)
+
+def get_student_score() -> Dict[str, float]:
+    return { "gaeun" : "100.0" }
+
+def find_user(user_id: int) -> Optional[str]:
+    users = {1 : "a", 2 : "b", 3: "c"}
+    return users.get(user_id)
+
+def function(value: Union[str, int]) -> int:
+    if (isinstance(value, int)):
+        return value ** 2
+    return len(value)
+
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(add("1", "2"))
+    print(process_number([1, 2, 3]))
+    print(get_person_info())
+    print(get_student_score())
+    print(find_user(1))
+    print(find_user(8))
+    print(function(2))
+    print(function("hello world"))
+
+# 3
+# 12
+# [2, 4, 6]
+# ('Park', 18)
+# {'gaeun': '100.0'}
+# a
+# None
+# 4
+# 11
