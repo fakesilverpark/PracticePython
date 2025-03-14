46318fc3fe84abe588f6b2ecc85cbd5767f41c17
eunbb <fakesilverpark@gmail.com>
2025-03-14 23:40:50 +0900

    2차시 수업 복습 + 주석

--- pr01/practice1.py
+++ pr01/practice1.py
@@ -5,23 +5,35 @@ app = FastAPI()
 @app.get("/hi1")
 def greet1():
     return "hello world"
-
+# url: http://127.0.0.1:8000/hi1
+# return: hello world
 @app.get("/hi2/{who}")
 def greet2(who: str):
     return f"{who}, hello world"
+# url: http://127.0.0.1:8000/hi2/gaeun
+# return: gaeun, hello world
 
 @app.get("/hi3")
 def greet3(who):
     return f"{who}, hello world"
+# url: http://127.0.0.1:8000/hi3?who=gaeun
+# params: key - who, value - gaeun
+# return: gaeun, hello world
 
-@app.get("/hi4")
+@app.post("/hi4")
 def greet4(who: str = Body(embed = True)):
     return f"{who}, hello world"
+# url: http://127.0.0.1:8000/hi4
+# body: { "who" : "gaeun" }
+# return: gaeun, hello world
 
 @app.get("/hi5")
 def greet5(who: str = Header()):
     return f"{who}, hello world"
+# url: http://127.0.0.1:8000/hi5
+# header:  key - who, value - gaeun
+# return: gaeun, hello world
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("hello:app", reload=True)
+    uvicorn.run("practice1:app", reload=True)
