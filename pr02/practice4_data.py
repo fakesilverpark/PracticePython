26b727b388b33ddc9e9e9d2564a68309c191c743
eunbb <fakesilverpark@gmail.com>
2025-03-16 18:22:34 +0900

    2차시 수업 복습6 id 로 객체 리턴 받는 코드 구현

--- pr02/practice4_data.py
+++ pr02/practice4_data.py
@@ -1,21 +1,36 @@
-from typing import List
+from typing import List, Union
 
 from practice.pr02.practice4_model import Champion, RoleEnum
 
 _champions = [
     Champion(
-        id=1,
+        id=2,
         name="Champion1",
         release_date="2025-03-22",
         role=RoleEnum.ASSASSIN
     ),
     Champion(
-            id=2,
+            id=1,
             name="Champion2",
             release_date="2025-03-22",
             role=RoleEnum.FIGHTER
     )
 ]
 
-def get_champion() -> List[Champion]:
+def get_all_champions() -> List[Champion]:
     return _champions
+
+# 매개변수로 온 아이디가 리스트에 존재하면 해당 객체를 리턴
+# 없으면 None
+def get_champion(champion_id: int) -> Union[Champion, str]:
+    # 구현하기2 : next()
+    # 못찾으면 StopIteration 예외 발생
+    # champion = next(champion for champion in _champions if champion.id == champion_id)
+    # return champion
+
+    # 하지만 이 코드는 없어도 예외가 발생하면 안됨!!
+    # 예외방지를 위하여 기본값 설정가능
+    champion = next((champion for champion in _champions if champion.id == champion_id), None)
+    if champion is None:
+        return "Champion not found"
+    return champion
--- pr02/practice4_web.py
+++ pr02/practice4_web.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 
-from ch02.data import get_champion
+from practice.pr02.practice4_data import get_all_champions, get_champion
 
 app = FastAPI()
 
 @app.get("/champions")
 def all_champions():
-    return get_champion()
+    return get_all_champions()
+
+@app.get("/champions/{champion_id}")
+def champion(champion_id: int):
+    return get_champion(champion_id)
 
 if __name__ == "__main__":
     import uvicorn
