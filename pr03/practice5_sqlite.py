d202352913701df3f29d7ea8172b6c34c8f8275e
eunbb <fakesilverpark@gmail.com>
2025-03-20 23:53:28 +0900

    3차시 수업 복습 테이블에 값 추가

--- pr03/practice5_sqlite.py
+++ pr03/practice5_sqlite.py
@@ -14,3 +14,9 @@ cur.execute("""
 # if not exists: 를 붙이면 해당 테이블이 이미 존재한다면
 # 테이블을 더 이상 생성하지 않는다
 # -> 오류 방지
+
+# 값 추가하기 (insert)
+cur.execute("insert into person(name, age) values (?, ?)", ("gaeun", 18))
+name = "eunbb" ; age = 22
+cur.execute(f'insert into person(name, age) values ("{name}", {age})')
+con.commit()
