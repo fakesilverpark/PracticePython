1d8aefc003b856ec4eb64ff6c7072bd8983d36d7
eunbb <fakesilverpark@gmail.com>
2025-03-22 20:32:27 +0900

    3차시 수업 복습 유저 조회 후 의존성 리턴

--- pr03/practice5_depends.py
+++ pr03/practice5_depends.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
 
 app = FastAPI()
 
+# 1. 유저 조회 후 의존성 리턴
+def user_dep(name: str = Query(...), gender: str = Query(...)):
+    return {"name": name, "gender": gender}
+
+@app.get("/user")
+def get_user(user: dict = Depends(user_dep)) -> dict:
+    return user
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("depends:app", reload=True)
+    uvicorn.run("practice5_depends:app", reload=True)
