3104be51047f876e888e96c68ea85ef8e94fd2df
eunbb <fakesilverpark@gmail.com>
2025-03-23 23:17:27 +0900

    3차시 수업 복습 데이터베이스 연결을 의존성으로 사용하기

--- pr03/practice5_depends.py
+++ pr03/practice5_depends.py
@@ -20,6 +20,23 @@ def check_admin(token: str = Query(...)):
 def check_admin(user: dict = Depends(check_admin)):
     return {"message" : "welcome", "user": user}
 
+# 3. 데이터베이스 연뎔을 의존성으로 사용하기
+class Database:
+    # 객체를 생성할 때 데이터베이스 연결
+    def __init__(self):
+        self.connection = "db connect"
+
+    def get_connect(self):
+        return self.connection
+
+def get_db():
+    db = Database()
+    return db.get_connect()
+
+@app.get("/db")
+def read_db(connection: str = Depends(get_db)):
+    return {"db_connect": connection}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("practice5_depends:app", reload=True)
