bce20d30b792630c760f9747a7491093f4a5bdea
eunbb <fakesilverpark@gmail.com>
2025-03-29 19:26:00 +0900

    응용프로그래밍 수업 3차시 복습

---
+++ pr04/practice6_data/__init__.py
@@ -0,0 +1,15 @@
+from sqlite3 import Connection, Cursor, connect
+from typing import Optional
+
+con: Optional[Connection] = None
+cur: Optional[Cursor] = None
+
+def get_db():
+    global con, cur
+
+    if con is None:
+        print("Connection")
+        con = connect("test.db")
+        cur = con.cursor()
+
+get_db()
